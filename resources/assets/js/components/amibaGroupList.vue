61a6d1fa3f11110d2eb0b8420c755b7c5f658c19
ggoop <samuel_abe@163.com>
2017-09-23 18:27:52 +0800

    init

--- resources/assets/js/components/amibaGroupList.vue
+++ resources/assets/js/components/amibaGroupList.vue
@@ -6,6 +6,15 @@
         <md-button @click.native="remove" :disabled="!(selectRows&&selectRows.length)">删除</md-button>
       </md-part-toolbar-group>
       <span class="flex"></span>
+      <md-part-toolbar-group>
+        <md-layout md-gutter>
+          <md-layout>
+            <md-input-container class="md-inset">
+              <md-input :fetch="doFetch" placeholder="search" @keyup.enter.native="load()"></md-input>
+            </md-input-container>
+          </md-layout>
+        </md-layout>
+      </md-part-toolbar-group>
       <md-part-toolbar-crumbs>
         <md-part-toolbar-crumb>阿米巴单元</md-part-toolbar-crumb>
         <md-part-toolbar-crumb>列表</md-part-toolbar-crumb>
@@ -18,45 +27,63 @@
   </md-part>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        selectRows: [],
-        loading:0
-      };
+export default {
+  data() {
+    return {
+      selectRows: [],
+      loading: 0
+    };
+  },
+  methods: {
+    create() {
+      this.$router.push({ name: 'module', params: { module: 'amiba.group.edit' } });
     },
-    methods: {
-      create(){
-        this.$router.push({ name: 'module', params: { module: 'amiba.group.edit' }});
-      },
-      edit(item){
-        this.$router.push({ name: 'id', params: { module: 'amiba.group.edit',id:item.id }});
-      },
-      remove(){
-        if(!this.selectRows||!this.selectRows.length){
-          this.$toast(this.$lang.LANG_NODELETEDATA);
-          return;
-        }
-        this.loading++;
-        const ids=this._.map(this.selectRows,'id').toString();
-        this.$http.delete('amiba/groups/'+ids).then(response => {
-          this.load();
-          this.loading--;
-          this.$toast(this.$lang.LANG_DELETESUCCESS);
-        }, response => {
-          this.$toast(this.$lang.LANG_DELETEFAIL);
-          this.loading--;
-        });
-      },
-      select(items){
-        this.selectRows=items;
-      },
-      load(){
-        this.$refs.list.pagination(1);
-      },
-      init_model_query(options){
-        options.orders[0]={name:'code'};
-      },
-    }
-  };
+    edit(item) {
+      this.$router.push({ name: 'id', params: { module: 'amiba.group.edit', id: item.id } });
+    },
+    doFetch(q) {
+      if (this.currentQ != q) {
+        this.currentQ = q;
+        this.load();
+      }
+      this.currentQ = q;
+    },
+    initQuery(options) {
+      options.wheres.filter = false;
+      if (this.currentQ) {
+        options.wheres.filter = {
+          "or": [
+            { name: 'code', operator: 'like', value: this.currentQ },
+            { name: 'name', operator: 'like', value: this.currentQ }
+          ]
+        };
+      }
+    },
+    remove() {
+      if (!this.selectRows || !this.selectRows.length) {
+        this.$toast(this.$lang.LANG_NODELETEDATA);
+        return;
+      }
+      this.loading++;
+      const ids = this._.map(this.selectRows, 'id').toString();
+      this.$http.delete('amiba/groups/' + ids).then(response => {
+        this.load();
+        this.loading--;
+        this.$toast(this.$lang.LANG_DELETESUCCESS);
+      }, response => {
+        this.$toast(this.$lang.LANG_DELETEFAIL);
+        this.loading--;
+      });
+    },
+    select(items) {
+      this.selectRows = items;
+    },
+    load() {
+      this.$refs.list.pagination(1);
+    },
+    init_model_query(options) {
+      options.orders[0] = { name: 'code' };
+    },
+  }
+};
 </script>
