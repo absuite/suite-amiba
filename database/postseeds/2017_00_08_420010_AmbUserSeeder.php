8ea4a1ec3270566221e58edfc260309334f48dfc
ggoop <samuel_abe@163.com>
2017-09-23 22:48:36 +0800

    init

--- database/postseeds/2017_00_08_420010_AmbUserSeeder.php
+++ database/postseeds/2017_00_08_420010_AmbUserSeeder.php
@@ -2,6 +2,7 @@
 
 use Gmf\Ac\Models\User;
 use Gmf\Sys\Builder;
+use Gmf\Sys\Models\Ent;
 use Illuminate\Database\Seeder;
 
 class AmbUserSeeder extends Seeder {
@@ -13,11 +14,14 @@ class AmbUserSeeder extends Seeder {
 	public function run() {
 
 		$b = new Builder;
-
+		$eid = config('gmf.ent.id');
 		for ($i = 1; $i <= 10; $i++) {
 			$b = new Builder;
 			$b->account('amb' . $i . '@amb.com')->name('amb' . $i . '@amb.com')->password('amb' . $i . '@amb.com');
-			User::registerByAccount('sys', $b->toArray());
+			$user = User::registerByAccount('sys', $b->toArray());
+
+			Ent::addUser($eid, $user->id);
+
 		}
 
 	}
