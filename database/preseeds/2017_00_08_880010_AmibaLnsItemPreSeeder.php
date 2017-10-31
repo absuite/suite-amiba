90b1a928c7b21c4a6d21316b7cff4a5f222111c8
pull user <pull user@163.com>
2017-10-31 23:03:40 +0800

    init

---
+++ database/preseeds/2017_00_08_880010_AmibaLnsItemPreSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Gmf\Sys\Builder;
+use Gmf\Sys\Models\LnsItem;
+use Illuminate\Database\Seeder;
+use Suite\Amiba\Models\Group;
+
+class AmibaLnsItemPreSeeder extends Seeder {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		LnsItem::build(function (Builder $b) {
+			$b->type(Group::class)->code('amiba')->name('阿米巴数');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+
+	}
+}
