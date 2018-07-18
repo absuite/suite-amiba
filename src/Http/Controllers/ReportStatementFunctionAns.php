bac19741c8d78856d66d4a9cefd97092d1f181cd
suite <amiysd@163.com>
2018-07-18 23:37:49 +0800

    init

--- src/Http/Controllers/ReportStatementFunctionAns.php
+++ src/Http/Controllers/ReportStatementFunctionAns.php
@@ -10,273 +10,134 @@ use Illuminate\Support\Facades\Log;
 use Suite\Amiba\Libs\QueryHelper;
 
 class ReportStatementFunctionAns extends Controller {
-	public function index(Request $request) {
-		Log::error(static::class);
-		$result = [];
-		$monthData = [];
-		$timeData = false;
-		$qc = QueryCase::formatRequest($request);
-		$period = QueryHelper::getPeriod($qc);
-		$group = QueryHelper::getGroup($qc);
-		if ($period && $group) {
+  public function index(Request $request) {
+    Log::error(static::class);
+    $result = [];
+    $monthData = [];
+    $timeData = false;
+    $qc = QueryCase::formatRequest($request);
+    $period = QueryHelper::getPeriod($qc);
+    $group = QueryHelper::getGroup($qc);
+    if (empty($period) || empty($group)) {
+      return $this->toJson(false);
+    }
 
-			$query = DB::table('suite_amiba_result_accounts as l');
-			$query->join('suite_cbo_period_accounts as p', 'l.period_id', '=', 'p.id');
-			$query->join('suite_amiba_elements as e', 'l.element_id', '=', 'e.id');
+    $query = DB::table('suite_amiba_result_accounts as l');
+    $query->join('suite_cbo_period_accounts as p', 'l.period_id', '=', 'p.id');
+    $query->join('suite_amiba_elements as e', 'l.element_id', '=', 'e.id');
 
-			$query->addSelect('e.name as elementName');
-			$query->addSelect('e.direction_enum as elementDirection');
-			$query->addSelect('e.type_enum as elementType');
-			$query->addSelect('e.is_manual as element_is_manual');
-			$query->addSelect('e.scope_enum as dataType');
+    $query->addSelect('e.id as element_id');
+    $query->addSelect(DB::raw("SUM(CASE WHEN p.id='" . $period->id . "' THEN 1  ELSE 0 END * l.money) AS month_value"));
+    $query->addSelect(DB::raw("SUM(l.money) as year_value"));
 
-			$query->addSelect(DB::raw("SUM(CASE WHEN p.id='" . $period->id . "' THEN 1  ELSE 0 END * l.money) AS money"));
-			$query->addSelect(DB::raw("SUM(l.money) as year_money"));
+    foreach ($qc->wheres as $key => $value) {
+      if ($value->name == 'group_id') {
+        QueryCase::attachWhere($query, $value, 'l.' . $value->name);
+      } else if ($value->name == 'purpose_id') {
+        QueryCase::attachWhere($query, $value, 'l.' . $value->name);
+      }
+    }
 
-			foreach ($qc->wheres as $key => $value) {
-				if ($value->name == 'group_id') {
-					QueryCase::attachWhere($query, $value, 'l.' . $value->name);
-				} else if ($value->name == 'purpose_id') {
-					QueryCase::attachWhere($query, $value, 'l.' . $value->name);
-				}
-			}
+    $query->where('p.year', '=', $period->year);
+    $query->where('p.from_date', '<=', $period->from_date);
 
-			$query->where('p.year', '=', $period->year);
-			$query->where('p.from_date', '<=', $period->from_date);
+    $query->groupBy('e.id');
+    $monthData = $query->get();
+    //elements 数据
+    $query = DB::table('suite_amiba_elements as l');
+    $query->addSelect('l.id');
+    $query->addSelect('l.parent_id');
+    $query->addSelect('l.name as itemName');
+    $query->addSelect('l.direction_enum as direction_enum');
+    $query->addSelect('l.type_enum as type_enum');
+    $query->addSelect('l.is_manual as is_manual');
+    $query->addSelect('l.scope_enum as scope_enum');
 
-			$query->groupBy('e.name', 'e.direction_enum', 'e.type_enum', 'e.scope_enum', 'e.is_manual');
-			$monthData = $query->get();
+    foreach ($qc->wheres as $key => $value) {
+      if ($value->name == 'purpose_id') {
+        QueryCase::attachWhere($query, $value, 'l.' . $value->name);
+      }
+    }
+    $query->orderBy('l.code');
+    $elements = $query->get();
+    $elements = $elements->each(function ($item) use ($monthData) {
+      $monthData->each(function ($im) use ($item) {
+        if ($im->element_id == $item->id) {
+          $item->month_value = $im->month_value;
+          $item->year_value = $im->year_value;
+        }
+      });
+		});
 
-			/*时间数据*/
-			$query = DB::table('suite_amiba_data_time_lines as l');
-			$query->join('suite_amiba_data_times as t', 'l.time_id', '=', 't.id');
-			$query->join('suite_cbo_period_accounts as p', 't.period_id', '=', 'p.id');
+		$rootNode =new \StdClass;
+		$rootNode ->nodes= QueryHelper::buildTree($elements);
 
-			$query->addSelect(DB::raw("SUM(CASE WHEN p.id='" . $period->id . "' THEN 1  ELSE 0 END * l.total_time) AS time_month"));
-			$query->addSelect(DB::raw("SUM(l.total_time) as time_year"));
+    /*时间数据*/
+    $query = DB::table('suite_amiba_data_time_lines as l');
+    $query->join('suite_amiba_data_times as t', 'l.time_id', '=', 't.id');
+    $query->join('suite_cbo_period_accounts as p', 't.period_id', '=', 'p.id');
 
-			foreach ($qc->wheres as $key => $value) {
-				if ($value->name == 'group_id') {
-					QueryCase::attachWhere($query, $value, 'l.' . $value->name);
-				} else if ($value->name == 'purpose_id') {
-					QueryCase::attachWhere($query, $value, 't.' . $value->name);
-				}
-			}
-			$query->where('p.year', '=', $period->year);
-			$query->where('p.from_date', '<=', $period->from_date);
+    $query->addSelect(DB::raw("SUM(CASE WHEN p.id='" . $period->id . "' THEN 1  ELSE 0 END * l.total_time) AS time_month"));
+    $query->addSelect(DB::raw("SUM(l.total_time) as time_year"));
 
-			$timeData = $query->first();
+    foreach ($qc->wheres as $key => $value) {
+      if ($value->name == 'group_id') {
+        QueryCase::attachWhere($query, $value, 'l.' . $value->name);
+      } else if ($value->name == 'purpose_id') {
+        QueryCase::attachWhere($query, $value, 't.' . $value->name);
+      }
+    }
+    $query->where('p.year', '=', $period->year);
+    $query->where('p.from_date', '<=', $period->from_date);
+
+    $timeData = $query->first();
+
+    //汇总上级
+		QueryHelper::sumTreeNodes($rootNode, ['month_value', 'year_value']);
+		QueryHelper::itemRatioTreeNodes($rootNode,'month_ratio','month_value');
+		QueryHelper::itemRatioTreeNodes($rootNode,'year_ratio','year_value');
+		foreach ($rootNode->nodes as $key => $value) {
+			
+			$value->month_ratio=empty( $value->month_value)?'0.00%':"100%";
+			$value->year_ratio=empty( $value->year_value)?'0.00%':"100%";
 		}
 
-		//收入
-		$item_rcv = new Builder;
-		$item_rcv->itemName('收入')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(0);
-
-		$item_rcv_out = new Builder;
-		$item_rcv_out->itemName('外部收入')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(1);
-
-		$item_rcv_in = new Builder;
-		$item_rcv_in->itemName('内部收入')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(1);
-
-		//成本
-		$item_cost = new Builder;
-		$item_cost->itemName('成本')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(0);
-
-		$item_cost_in = new Builder;
-		$item_cost_in->itemName('内采成本')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(1);
-
-		$item_cost_out = new Builder;
-		$item_cost_out->itemName('外部成本')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(1);
-
-		//税金及附加
-		$item_tax = new Builder;
-		$item_tax->itemName('税金及附加')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(0);
-
-		$item_free = new Builder;
-		$item_free->itemName('费用')->direction('－')->month_value(0)->year_value(0)->nodes([])->indent(0);
-
-		foreach ($monthData as $key => $v) {
-			$item = new Builder;
-			$item->itemName($v->elementName)->month_value($v->money)->year_value($v->year_money)->indent(2);
-			if ($v->elementDirection == 'decrease') {
-				$tv = 0 - $v->money;
-				$year_tv = 0 - $v->year_money;
-				$item->direction('减');
-			} else {
-				$tv = $v->money;
-				$year_tv = $v->year_money;
-				$item->direction('增');
-			}
-
-			if ($v->elementType == 'rcv') {
-				if ($v->dataType == 'outside') {
-					$array = $item_rcv_out->nodes;
-					$array[] = $item;
-					$item_rcv_out->nodes = $array;
-				} else {
-					$array = $item_rcv_in->nodes;
-					$array[] = $item;
-					$item_rcv_in->nodes = $array;
-				}
-			}
-			if ($v->elementType == 'cost') {
-				if ($v->dataType == 'outside') {
-					$array = $item_cost_out->nodes;
-					$array[] = $item;
-					$item_cost_out->nodes = $array;
-				} else {
-					$array = $item_cost_in->nodes;
-					$array[] = $item;
-					$item_cost_in->nodes = $array;
-				}
-			}
-			if ($v->elementType == 'free') {
-				$array = $item_free->nodes;
-				$array[] = $item;
-				$item_free->nodes = $array;
-			}
-			if ($v->elementType == 'tax') {
-				$array = $item_tax->nodes;
-				$array[] = $item;
-				$item_tax->nodes = $array;
-			}
-		}
-
-		$array = $item_rcv->nodes;
-		$array[] = $item_rcv_out;
-		$array[] = $item_rcv_in;
-		$item_rcv->nodes = $array;
-
-		$array = $item_cost->nodes;
-		$array[] = $item_cost_in;
-		$array[] = $item_cost_out;
-		$item_cost->nodes = $array;
-
-		//汇总上级
-		QueryHelper::sumTreeNodes($item_rcv, ['month_value', 'year_value']);
-		QueryHelper::sumTreeNodes($item_cost, ['month_value', 'year_value']);
-		QueryHelper::sumTreeNodes($item_tax, ['month_value', 'year_value']);
-		QueryHelper::sumTreeNodes($item_free, ['month_value', 'year_value']);
-
-		//主营利润，收入合计－成本合计－税金
-		$item_profit = new Builder;
-		$item_profit->itemName('主营利润')->direction('－')->month_value($item_rcv->month_value - $item_cost->month_value - $item_tax->month_value)->indent(0);
-		$item_profit->year_value($item_rcv->year_value - $item_cost->year_value - $item_tax->year_value);
+    //主营利润，收入合计－成本合计－税金
+    $item_profit = new Builder;
+		$item_profit->itemName('主营利润')->month_value(0)->year_value(0);
+		
 		//净利润，主营利润-费用
-		$item_net_profit = new Builder;
-		$item_net_profit->itemName('净利润')->direction('－')->month_value($item_profit->month_value - $item_free->month_value)->indent(0);
-		$item_net_profit->year_value($item_profit->year_value - $item_free->year_value);
+    $item_net_profit = new Builder;
+		$item_net_profit->itemName('净利润')->month_value(0)->year_value(0);
+		
+    foreach ($rootNode->nodes as $key => $value) {
+      if ($value->type_enum == 'rcv') {
+				$item_profit->month_value = $item_profit->month_value + (empty($value->month_value) ? 0 : $value->month_value);
+				$item_profit->year_value = $item_profit->year_value + (empty($value->year_value) ? 0 : $value->year_value);
 
-		array_push($result, $item_rcv);
-		QueryHelper::appendNodesToArray($result, $item_rcv);
-		array_push($result, $item_cost);
-		QueryHelper::appendNodesToArray($result, $item_cost);
-		array_push($result, $item_tax);
-		QueryHelper::appendNodesToArray($result, $item_tax);
-		array_push($result, $item_profit);
-		QueryHelper::appendNodesToArray($result, $item_profit);
-		array_push($result, $item_free);
-		QueryHelper::appendNodesToArray($result, $item_free);
-		array_push($result, $item_net_profit);
-		QueryHelper::appendNodesToArray($result, $item_net_profit);
-
-		foreach ($result as $key => $value) {
-			if ($item_rcv->month_value != 0) {
-				$value->month_ratio(round($value->month_value * 100 / $item_rcv->month_value, 2) . "%");
-			} else {
-				$value->month_ratio("0%");
+				$item_net_profit->month_value = $item_net_profit->month_value + (empty($value->month_value) ? 0 : $value->month_value);
+				$item_net_profit->year_value = $item_net_profit->year_value + (empty($value->year_value) ? 0 : $value->year_value);
 			}
-			if ($item_rcv->year_value != 0) {
-				$value->year_ratio(round($value->year_value * 100 / $item_rcv->year_value, 2) . "%");
-			} else {
-				$value->year_ratio("0%");
+			if ($value->type_enum == 'cost'||$value->type_enum == 'tax') {
+				$item_profit->month_value = $item_profit->month_value - (empty($value->month_value) ? 0 : $value->month_value);
+				$item_profit->year_value = $item_profit->year_value - (empty($value->year_value) ? 0 : $value->year_value);
+
+				$item_net_profit->month_value = $item_net_profit->month_value - (empty($value->month_value) ? 0 : $value->month_value);
+				$item_net_profit->year_value = $item_net_profit->year_value - (empty($value->year_value) ? 0 : $value->year_value);
 			}
+			if ($value->type_enum == 'free') {
+				$item_net_profit->month_value = $item_net_profit->month_value - (empty($value->month_value) ? 0 : $value->month_value);
+				$item_net_profit->year_value = $item_net_profit->year_value - (empty($value->year_value) ? 0 : $value->year_value);
+      }
 		}
-		$totalManel = 0;
-		if ($monthData) {
-			foreach ($monthData as $key => $v) {
-				if ($v->element_is_manual) {
-					$totalManel += $v->money;
-				}
-			}
-		}
+		$rootNode->nodes[]=$item_profit;
+		$rootNode->nodes[]=$item_net_profit;
+		
 
-		//收入/劳动人数
-		$item = new Builder;
-		$item->itemName('劳动人数')->direction('－')->month_value(0)->indent(0);
-		if ($group && $group->employees) {
-			$item->month_value($group->employees)->month_ratio(round($item_rcv->month_value / $group->employees, 2));
-		}
-		array_push($result, $item);
+		$result=[];
+		
+    QueryHelper::appendNodesToArray($result,$rootNode);
 
-		//劳动时间=收入/劳动时间
-		$item = new Builder;
-		$item->itemName('劳动时间')->direction('－')->month_value(0)->indent(0);
-		if ($timeData && $timeData->time_month != 0) {
-			$item->month_value($timeData->time_month)->month_ratio(round($item_rcv->month_value / $timeData->time_month, 2));
-		}
-		array_push($result, $item);
-
-		//人工成本=收入/人工成本
-		$item = new Builder;
-		$item->itemName('人工成本')->direction('－')->month_value(0)->indent(0);
-		if ($totalManel != 0) {
-			$item->month_value($totalManel)->month_ratio(round($item_rcv->month_value / $totalManel, 2));
-		}
-		array_push($result, $item);
-
-		//人均毛利=毛利/劳动人数
-		$item = new Builder;
-		$item->itemName('人均毛利')->direction('－')->month_value(0)->indent(0);
-		if ($group && $group->employees && $item_profit) {
-			$item->month_value(round($item_profit->month_value / $group->employees, 2));
-		}
-		array_push($result, $item);
-
-		//人均净利=净利/劳动人数
-		$item = new Builder;
-		$item->itemName('人均净利')->direction('－')->month_value(0)->indent(0);
-		if ($group && $group->employees && $item_net_profit) {
-			$item->month_value(round($item_net_profit->month_value / $group->employees, 2));
-		}
-		array_push($result, $item);
-
-		//工作效率=毛利/劳动时间
-		$item = new Builder;
-		$item->itemName('工作效率')->direction('－')->month_value(0)->indent(0);
-		if ($timeData && $timeData->time_month != 0 && $item_profit) {
-			$item->month_value(round($item_profit->month_value / $timeData->time_month, 2));
-		}
-		array_push($result, $item);
-
-		//经营效率=净利/劳动时间
-		$item = new Builder;
-		$item->itemName('经营效率')->direction('－')->month_value(0)->indent(0);
-		if ($timeData && $timeData->time_month != 0 && $item_net_profit) {
-			$item->month_value(round($item_net_profit->month_value / $timeData->time_month, 2));
-		}
-		array_push($result, $item);
-
-		//工作效益=毛利/人工成本
-		$item = new Builder;
-		$item->itemName('工作效益')->direction('－')->month_value(0)->indent(0);
-		if ($totalManel) {
-			$item->month_value(round($item_profit->month_value / $totalManel, 2));
-		}
-		array_push($result, $item);
-
-		//经营效益=净利/人工成本
-		$item = new Builder;
-		$item->itemName('经营效益')->direction('－')->month_value(0)->indent(0);
-		if ($totalManel) {
-			$item->month_value(round($item_net_profit->month_value / $totalManel, 2));
-		}
-		array_push($result, $item);
-
-		foreach ($result as $key => $value) {
-			$value->nodes([]);
-		}
-
-		return $this->toJson($result);
-	}
+    return $this->toJson($result);
+  }
 }
--- src/Libs/QueryHelper.php
+++ src/Libs/QueryHelper.php
@@ -3,200 +3,238 @@
 namespace Suite\Amiba\Libs;
 use Carbon\Carbon;
 use DB;
+use Gmf\Sys\Builder;
 use Gmf\Sys\Query\QueryCase;
 
 class QueryHelper {
-	public static function getPeriod($queryCase) {
-		$purposeId = false;
-		$periodId = false;
-		foreach ($queryCase->wheres as $key => $value) {
-			if ($value->name == 'purpose_id') {
-				$purposeId = $value->value;
-			} else if ($value->name == 'period_id') {
-				$periodId = $value->value;
-			}
-		}
-		$query = DB::table('suite_cbo_period_accounts as a');
-		$query->addSelect('a.id');
-		$query->addSelect('a.name');
-		$query->addSelect('a.year');
-		$query->addSelect('a.month');
-		$query->addSelect('a.to_date');
-		$query->addSelect('a.from_date');
+  public static function getPeriod($queryCase) {
+    $purposeId = false;
+    $periodId = false;
+    foreach ($queryCase->wheres as $key => $value) {
+      if ($value->name == 'purpose_id') {
+        $purposeId = $value->value;
+      } else if ($value->name == 'period_id') {
+        $periodId = $value->value;
+      }
+    }
+    $query = DB::table('suite_cbo_period_accounts as a');
+    $query->addSelect('a.id');
+    $query->addSelect('a.name');
+    $query->addSelect('a.year');
+    $query->addSelect('a.month');
+    $query->addSelect('a.to_date');
+    $query->addSelect('a.from_date');
 
-		if ($purposeId) {
-			$query->join('suite_amiba_purposes  as p', 'p.calendar_id', '=', 'a.calendar_id');
-			$query->where('p.id', $purposeId);
-		}
-		if ($periodId) {
-			$query->where('a.id', $periodId);
-		} else {
-			$t = Carbon::now();
-			$date = $t->toDateString();
-			$query->where('a.to_date', '>=', $date);
-			$query->where('a.from_date', '<=', $date);
-		}
-		$query->orderBy('a.to_date');
-		$monthData = $query->first();
-		return $monthData;
+    if ($purposeId) {
+      $query->join('suite_amiba_purposes  as p', 'p.calendar_id', '=', 'a.calendar_id');
+      $query->where('p.id', $purposeId);
+    }
+    if ($periodId) {
+      $query->where('a.id', $periodId);
+    } else {
+      $t = Carbon::now();
+      $date = $t->toDateString();
+      $query->where('a.to_date', '>=', $date);
+      $query->where('a.from_date', '<=', $date);
+    }
+    $query->orderBy('a.to_date');
+    $monthData = $query->first();
+    return $monthData;
+  }
+  public static function getPrevPeriod($queryCase) {
+    $period = static::getPeriod($queryCase);
+
+    $query = DB::table('suite_cbo_period_accounts as a');
+    $query->addSelect('a.id');
+    $query->addSelect('a.name');
+    $query->addSelect('a.year');
+    $query->addSelect('a.month');
+    $query->addSelect('a.to_date');
+    $query->addSelect('a.from_date');
+    if ($period) {
+      $query->where('a.from_date', '<', $period->from_date);
+    }
+    $query->orderBy('a.to_date', 'desc');
+    $monthData = $query->first();
+
+    return $monthData;
+  }
+  public static function getPeriods($queryCase) {
+
+    $purposeId = false;
+    $fm_period = false;
+    $to_period = false;
+    foreach ($queryCase->wheres as $key => $value) {
+      if ($value->name == 'purpose_id') {
+        $purposeId = $value->value;
+      } else if ($value->name == 'fm_period') {
+        $fm_period = $value->value;
+      } else if ($value->name == 'to_period') {
+        $to_period = $value->value;
+      }
+    }
+
+    $t = Carbon::now();
+    $date = $t->toDateString();
+
+    $query = DB::table('suite_cbo_period_accounts as a');
+    $query->addSelect('a.id');
+    $query->addSelect('a.name');
+    $query->addSelect('a.year');
+    $query->addSelect('a.month');
+    $query->addSelect('a.to_date');
+    $query->addSelect('a.from_date');
+
+    if ($purposeId) {
+      $query->join('suite_amiba_purposes  as p', 'p.calendar_id', '=', 'a.calendar_id');
+      $query->where('p.id', $purposeId);
+    }
+    if ($fm_period) {
+      $query->where('a.code', '>=', $fm_period);
+    } else {
+      $query->where('a.to_date', '>=', $date);
+    }
+    if ($to_period) {
+      $query->where('a.code', '<=', $to_period);
+    } else {
+      $query->where('a.from_date', '<=', $date);
+    }
+    $query->orderBy('a.to_date');
+    $datas = $query->get();
+    if ($datas && count($datas)) {
+      return $datas;
+    }
+    return false;
+  }
+  public static function getGroups($queryCase, $is_leaf = false) {
+
+    $hasGroupIds = false;
+    $query = DB::table('suite_amiba_groups as l');
+    $query->addSelect('l.id');
+    $query->addSelect('l.name');
+    $query->addSelect('l.is_leaf');
+    $query->addSelect('l.employees');
+    if ($is_leaf) {
+      $hasGroupIds = true;
+      $query->where('l.is_leaf', '1');
+    }
+    foreach ($queryCase->wheres as $key => $value) {
+      if ($value->name == 'purpose_id') {
+        QueryCase::attachWhere($query, $value, 'l.purpose_id');
+      } else if ($value->name == 'group_id') {
+        $hasGroupIds = true;
+        QueryCase::attachWhere($query, $value, 'l.id');
+      }
+    }
+    if (!$hasGroupIds) {
+      $query->where('l.is_leaf', '1');
+    }
+    $query->orderBy('l.name');
+    $datas = $query->get();
+    if ($datas && count($datas)) {
+      return $datas;
+    }
+    return false;
+  }
+  public static function getGroup($queryCase, $is_leaf = false) {
+
+    $hasGroupIds = false;
+    $query = DB::table('suite_amiba_groups as l');
+    $query->addSelect('l.id');
+    $query->addSelect('l.name');
+    $query->addSelect('l.is_leaf');
+    $query->addSelect('l.employees');
+    if ($is_leaf) {
+      $hasGroupIds = true;
+      $query->where('l.is_leaf', '1');
+    }
+    foreach ($queryCase->wheres as $key => $value) {
+      if ($value->name == 'purpose_id') {
+        QueryCase::attachWhere($query, $value, 'l.purpose_id');
+      } else if ($value->name == 'group_id') {
+        $hasGroupIds = true;
+        QueryCase::attachWhere($query, $value, 'l.id');
+      }
+    }
+    if (!$hasGroupIds) {
+      $query->where('l.is_leaf', '1');
+    }
+    $query->orderBy('l.name');
+    $datas = $query->first();
+
+    return $datas;
+  }
+
+  public static function sumTreeNodes(&$tree, $sumFields = []) {
+    $tv = [];
+    foreach ($sumFields as $fk => $fv) {
+      $tv[$fv] = 0;
+    }
+    if (!empty($tree->nodes) && count($tree->nodes)) {
+      foreach ($tree->nodes as $nk => $nv) {
+        static::sumTreeNodes($nv, $sumFields);
+        foreach ($sumFields as $fk => $fv) {
+          $tv[$fv] = $tv[$fv] + (empty($nv->{$fv})?0: $nv->{$fv});
+        }
+      }
+    }
+    foreach ($sumFields as $fk => $fv) {
+      $tree->{$fv} = (empty($tree->{$fv})?0: $tree->{$fv})+ $tv[$fv];
+    }
+  }
+  public static function itemRatioTreeNodes(&$tree,$ratioField, $valueField) {
+    if (!empty($tree->nodes) && count($tree->nodes)) {
+      foreach ($tree->nodes as $nk => $nv) {
+        static::itemRatioTreeNodes($nv,$ratioField, $valueField);
+        if(!empty($tree->{$valueField})&&!empty($nv->{$valueField})){
+          $nv->{$ratioField}=round($nv->{$valueField}/$tree->{$valueField}*100,2).'%';
+        }else{
+          $nv->{$ratioField}='0.00%';
+        }
+      }
+    }
+  }
+  private static function buildTreeNodes($parentNode, $rows = []) {
+    $nodes = [];
+    foreach ($rows as $k => $v) {
+      if ($parentNode->id == $v->parent_id) {
+        static::buildTreeNodes($v, $rows);
+        $nodes[] = $v;
+      }
+    }
+    if (count($nodes)) {
+      $parentNode->nodes=$nodes;
+    }
+  }
+  public static function buildTree($rows = []) {
+    $tree = [];
+    foreach ($rows as $k => $v) {
+      if (empty($v->parent_id)) {
+        static::buildTreeNodes($v, $rows);
+        $tree[] = $v;
+      }
+    }
+    return count($tree) ? $tree : false;
 	}
-	public static function getPrevPeriod($queryCase) {
-		$period = static::getPeriod($queryCase);
-
-		$query = DB::table('suite_cbo_period_accounts as a');
-		$query->addSelect('a.id');
-		$query->addSelect('a.name');
-		$query->addSelect('a.year');
-		$query->addSelect('a.month');
-		$query->addSelect('a.to_date');
-		$query->addSelect('a.from_date');
-		if ($period) {
-			$query->where('a.from_date', '<', $period->from_date);
+  /**
+   * 把树型结构的数据平
+   * @param  array  $array [description]
+   * @param  [type] $item  [description]
+   * @return [type]        [description]
+   */
+  public static function appendNodesToArray(array &$array, $treeNode) {
+    if (empty($treeNode->nodes)) {
+      return;
 		}
-		$query->orderBy('a.to_date', 'desc');
-		$monthData = $query->first();
-
-		return $monthData;
-	}
-	public static function getPeriods($queryCase) {
-
-		$purposeId = false;
-		$fm_period = false;
-		$to_period = false;
-		foreach ($queryCase->wheres as $key => $value) {
-			if ($value->name == 'purpose_id') {
-				$purposeId = $value->value;
-			} else if ($value->name == 'fm_period') {
-				$fm_period = $value->value;
-			} else if ($value->name == 'to_period') {
-				$to_period = $value->value;
-			}
+		if(empty($treeNode->indent)){
+			$treeNode->indent=0;
 		}
-
-		$t = Carbon::now();
-		$date = $t->toDateString();
-
-		$query = DB::table('suite_cbo_period_accounts as a');
-		$query->addSelect('a.id');
-		$query->addSelect('a.name');
-		$query->addSelect('a.year');
-		$query->addSelect('a.month');
-		$query->addSelect('a.to_date');
-		$query->addSelect('a.from_date');
-
-		if ($purposeId) {
-			$query->join('suite_amiba_purposes  as p', 'p.calendar_id', '=', 'a.calendar_id');
-			$query->where('p.id', $purposeId);
-		}
-		if ($fm_period) {
-			$query->where('a.code', '>=', $fm_period);
-		} else {
-			$query->where('a.to_date', '>=', $date);
-		}
-		if ($to_period) {
-			$query->where('a.code', '<=', $to_period);
-		} else {
-			$query->where('a.from_date', '<=', $date);
-		}
-		$query->orderBy('a.to_date');
-		$datas = $query->get();
-		if ($datas && count($datas)) {
-			return $datas;
-		}
-		return false;
-	}
-	public static function getGroups($queryCase, $is_leaf = false) {
-
-		$hasGroupIds = false;
-		$query = DB::table('suite_amiba_groups as l');
-		$query->addSelect('l.id');
-		$query->addSelect('l.name');
-		$query->addSelect('l.is_leaf');
-		$query->addSelect('l.employees');
-		if ($is_leaf) {
-			$hasGroupIds = true;
-			$query->where('l.is_leaf', '1');
-		}
-		foreach ($queryCase->wheres as $key => $value) {
-			if ($value->name == 'purpose_id') {
-				QueryCase::attachWhere($query, $value, 'l.purpose_id');
-			} else if ($value->name == 'group_id') {
-				$hasGroupIds = true;
-				QueryCase::attachWhere($query, $value, 'l.id');
-			}
-		}
-		if (!$hasGroupIds) {
-			$query->where('l.is_leaf', '1');
-		}
-		$query->orderBy('l.name');
-		$datas = $query->get();
-		if ($datas && count($datas)) {
-			return $datas;
-		}
-		return false;
-	}
-	public static function getGroup($queryCase, $is_leaf = false) {
-
-		$hasGroupIds = false;
-		$query = DB::table('suite_amiba_groups as l');
-		$query->addSelect('l.id');
-		$query->addSelect('l.name');
-		$query->addSelect('l.is_leaf');
-		$query->addSelect('l.employees');
-		if ($is_leaf) {
-			$hasGroupIds = true;
-			$query->where('l.is_leaf', '1');
-		}
-		foreach ($queryCase->wheres as $key => $value) {
-			if ($value->name == 'purpose_id') {
-				QueryCase::attachWhere($query, $value, 'l.purpose_id');
-			} else if ($value->name == 'group_id') {
-				$hasGroupIds = true;
-				QueryCase::attachWhere($query, $value, 'l.id');
-			}
-		}
-		if (!$hasGroupIds) {
-			$query->where('l.is_leaf', '1');
-		}
-		$query->orderBy('l.name');
-		$datas = $query->first();
-
-		return $datas;
-	}
-
-	public static function sumTreeNodes($tree, $sumFields = []) {
-		$tv = [];
-		foreach ($sumFields as $fk => $fv) {
-			$tv[$fv] = 0;
-		}
-		if (!empty($tree->nodes) && count($tree->nodes)) {
-			foreach ($tree->nodes as $nk => $nv) {
-				static::sumTreeNodes($nv, $sumFields);
-				foreach ($sumFields as $fk => $fv) {
-					$tv[$fv] = $tv[$fv] + $nv->{$fv};
-				}
-			}
-			foreach ($sumFields as $fk => $fv) {
-				$tree->{$fv} = $tv[$fv];
-			}
-		}
-	}
-	/**
-	 * 把树型结构的数据平
-	 * @param  array  $array [description]
-	 * @param  [type] $item  [description]
-	 * @return [type]        [description]
-	 */
-	public static function appendNodesToArray(array &$array, $item) {
-		if (empty($item->nodes)) {
-			return;
-		}
-		foreach ($item->nodes as $key => $value) {
-			$value->indent($item->indent + 1);
-			array_push($array, $value);
-			if (!empty($value->nodes)) {
-				static::appendNodesToArray($array, $value);
-			}
-		}
-	}
+    foreach ($treeNode->nodes as $key => $value) {
+      $value->indent=$treeNode->indent + 1;
+      array_push($array, $value);
+      if (!empty($value->nodes)) {
+        static::appendNodesToArray($array, $value);
+      }
+    }
+  }
 }
