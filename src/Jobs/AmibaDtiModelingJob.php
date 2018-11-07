9cdacf67739e63af20f07b2fcac1db91ea454877
吴运刚 <wuyg@yonyou.com>
2018-11-07 18:30:02 +0800

    a

--- src/Jobs/AmibaDtiModelingJob.php
+++ src/Jobs/AmibaDtiModelingJob.php
@@ -9,8 +9,11 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Suite\Amiba\Models\DtiModeling;
+use Carbon\Carbon;
 
-class AmibaDtiModelingJob implements ShouldQueue {
+class AmibaDtiModelingJob implements ShouldQueue
+{
   use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
   protected $model;
   protected $period;
@@ -20,11 +23,13 @@ class AmibaDtiModelingJob implements ShouldQueue {
    *
    * @return void
    */
-  public function __construct($model, $period) {
+  public function __construct($model, $period)
+  {
     $this->model = $model;
     $this->period = $period;
   }
-  private function handleByRuntime() {
+  private function handleByRuntime()
+  {
     $base_uri = env("GMF_RUNTIME_HOST");
     $client = new GuzzleHttp\Client([
       'base_uri' => $base_uri,
@@ -40,14 +45,15 @@ class AmibaDtiModelingJob implements ShouldQueue {
     $res = $client->request('POST', "api/amiba/models/modeling", [
       'json' => $input,
     ]);
-    $result = (string) $res->getBody();
+    $result = (string)$res->getBody();
   }
   /**
    * Execute the job.
    *
    * @return void
    */
-  public function handle() {
+  public function handle()
+  {
     if (empty($this->model)) {
       return;
     }
@@ -57,12 +63,33 @@ class AmibaDtiModelingJob implements ShouldQueue {
         $this->handleByRuntime();
         return;
       }
-      DB::statement("CALL sp_amiba_data_modeling(?,?,?,?);", [$this->model->ent_id, $this->model->purpose_id, $this->period->period_id, $this->model->id]);
+      $m = DtiModeling::updateOrCreate([
+        "ent_id" => $this->model->ent_id,
+        "purpose_id" => $this->model->purpose_id,
+        "period_id" => $this->period->id,
+        "model_id" => $this->model->id
+      ], ["msg" => "开始执行"]);
+      $m->succeed = false;
+      $m->start_time = new Carbon;
+      $m->end_time = null;
+      $m->msg = null;
+      $m->save();
+
+      DB::statement("CALL sp_amiba_data_modeling(?,?,?,?);", [$this->model->ent_id, $this->model->purpose_id, $this->period->id, $this->model->id]);
     } catch (\Exception $ex) {
       $e = $ex;
       throw $ex;
-    } finally {
-
+    }
+    finally {
+      $m = DtiModeling::find($this->model->id);
+      if ($e) {
+        $m->succeed = false;
+        $m->msg = $e->getMessage();
+      } else {
+        $m->succeed = true;
+      }
+      $m->end_time = new Carbon;
+      $m->save();
     }
   }
 
